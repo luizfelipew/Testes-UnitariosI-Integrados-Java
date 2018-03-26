018d8d3908854f2e58f9aea61ec3a4b4f6d07b39
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-03-26 15:11:16 -0300

    Adicionando o JUnit

--- TestesUnitarios/src/main/java/br/ce/wendt/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wendt/servicos/LocacaoService.java
@@ -8,6 +8,8 @@ import br.ce.wendt.entidades.Filme;
 import br.ce.wendt.entidades.Locacao;
 import br.ce.wendt.entidades.Usuario;
 import br.ce.wendt.utils.DataUtils;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class LocacaoService {
 	
@@ -28,8 +30,9 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-	
-	public static void main(String[] args) {
+
+	@Test
+	public void teste() {
 		//cenario
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = new Usuario("Usuario 1");
@@ -39,8 +42,8 @@ public class LocacaoService {
 		Locacao locacao = service.alugarFilme(usuario, filme);
 		
 		//verificacao
-		System.out.println(locacao.getValor() == 5.0);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Assert.assertTrue(locacao.getValor() == 5.0);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
