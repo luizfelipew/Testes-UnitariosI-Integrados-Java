99506ba4162ac9c9af1ee6d20ff44ef818f403c4
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-03-27 17:14:10 -0300

    AssertThat

--- TestesUnitarios/src/test/java/br/ce/wendt/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wendt/servicos/LocacaoServiceTest.java
@@ -3,14 +3,17 @@ package br.ce.wendt.servicos;
 import br.ce.wendt.entidades.Filme;
 import br.ce.wendt.entidades.Locacao;
 import br.ce.wendt.entidades.Usuario;
-import br.ce.wendt.servicos.LocacaoService;
-import br.ce.wendt.utils.DataUtils;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
 
-import static br.ce.wendt.utils.DataUtils.adicionarDias;
+import static br.ce.wendt.utils.DataUtils.isMesmaData;
+import static br.ce.wendt.utils.DataUtils.obterDataComDiferencaDias;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
 
 public class LocacaoServiceTest {
 
@@ -26,8 +29,13 @@ public class LocacaoServiceTest {
 
 
 		//verificacao
-		Assert.assertEquals( 5.0, locacao.getValor(), 0.01);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		//Assert.assertEquals( 5.0, locacao.getValor(), 0.01);
+		//Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		//Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(5.0)));
+		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 	}
 }
