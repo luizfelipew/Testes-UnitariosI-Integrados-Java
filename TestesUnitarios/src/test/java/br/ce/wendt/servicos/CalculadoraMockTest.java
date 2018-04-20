121cf570a4296dc46d3c2aa18030c1ab572270bc
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2018-04-19 22:56:38 -0300

    Usando o Spy

--- TestesUnitarios/src/test/java/br/ce/wendt/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/ce/wendt/servicos/CalculadoraMockTest.java
@@ -1,12 +1,48 @@
 package br.ce.wendt.servicos;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
+import org.mockito.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class CalculadoraMockTest {
+
+
+    @Mock
+    private Calculadora calcMock;
+
+    @Spy
+    private Calculadora calcSpy;
+
+    @Mock
+    private EmailService email;
+
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void devoMostrarDiferencaEntreMockSpy(){
+       // Mockito.when(calcMock.somar(1,2)).thenReturn(8);
+       // Mockito.when(calcMock.somar(1,2)).thenCallRealMethod();
+        Mockito.when(calcMock.somar(1,2)).thenReturn(5);
+      //  Mockito.when(calcSpy.somar(1,2)).thenReturn(5); //da um problema pq executa oq ta dentro do parentes agora vamos arrumar
+        Mockito.doReturn(5).when(calcSpy).somar(1,2);
+        //para nao executar o spy
+        Mockito.doNothing().when(calcSpy).imprime();
+
+        System.out.println("Mock " + calcMock.somar(1,2));
+        System.out.println("Spy " + calcSpy.somar(1,2));
+
+
+        System.out.println("Mock !!");
+        calcMock.imprime();
+        System.out.println("Spy !!");  //comportamento padrao eh executar o metodo
+        calcSpy.imprime();
+    }
 
     @Test
     public void teste(){
@@ -16,6 +52,6 @@ public class CalculadoraMockTest {
         Mockito.when(calc.somar(argCapt.capture(), argCapt.capture())).thenReturn(5);
 
         assertEquals(5, calc.somar(1, 100000));
-        System.out.println(argCapt.getAllValues());
+     //   System.out.println(argCapt.getAllValues());
     }
 }
