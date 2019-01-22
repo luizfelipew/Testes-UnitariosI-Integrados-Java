da07f727a946c454e43473764e49c1891ca65c7e
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2019-01-21 23:02:55 -0200

    Estudos com schedule

---
+++ TestesUnitarios/src/main/java/br/ce/wendt/TaskRun.java
@@ -0,0 +1,14 @@
+package br.ce.wendt;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TaskRun {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskRun.class, args);
+    }
+}
--- TestesUnitarios/src/test/java/br/ce/wendt/utils/FileUtilsTest.java
+++ TestesUnitarios/src/test/java/br/ce/wendt/utils/FileUtilsTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class FIleUtilsTest {
+public class FileUtilsTest {
 
     private File diretorio;
 
